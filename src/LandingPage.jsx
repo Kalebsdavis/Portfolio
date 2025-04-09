a9bf529c44daca291a38b64b985ec46a8b45ae8c
Kaleb <kalebdavis8289@gmail.com>
2025-04-09 13:21:08 -0600

    added icons and animated cursor

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,14 +3,35 @@ import TrackMate from './TrackMate/TrackMate';
 import RV from './RV/RV';
 import BankApp from './BankApp/BankApp';
 import { Link } from 'react-router-dom';
+import { FaReact } from "react-icons/fa";
+import { FaNode } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import { DiDjango } from "react-icons/di";
+import { SiMongodb } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiSqlite } from "react-icons/si";
 
 const LandingPage = () =>{
     
     return(
     <div className='landing-page-background'>
         <div  className='landing-page-container'>
-            <div className='header'><Link to='https://www.linkedin.com/in/kaleb-davis-77037b336'><h1>Kaleb Davis</h1></Link></div>
+           <div className='About-me' style={{ width:'100vw', marginLeft:'5rem'}}>
+            <h2 style={{ fontWeight:'normal', fontSize:'40px',marginTop:'3rem', color:'white', textShadow:' 1px 1px 10px rgb(255, 255, 255)'}}>
+                I'm Kaleb Davis,<br/>
+                    A Full Stack Developer <span className='cursor'/>
+                </h2>
+            <p style={{marginTop:'1rem',  color:'white'}}>Austin, Texas</p>
+            <p style={{marginTop:'1rem',  color:'white'}}><FaReact size={20}/>React.js  <FaNode size={20}/>Node.js  <SiNextdotjs size={20}/>Next.js   <DiDjango size={20}/>Django   
+            <SiMongodb size={20}/>MongoDB   <GrMysql size={20} />MySQL  
+            <BiLogoPostgresql size={20} />PostgreSQL    <SiSqlite size={20} />SQLite
+            </p>
+             <h2 style={{fontWeight:'normal', fontSize:'30px',marginTop:'3rem', color:'white', textShadow:' 1px 1px 10px rgb(255, 255, 255)'}}>Projects</h2>
+           </div>
+          
             <div className='card-container'>
+            
             <div className='trackmate-card'><TrackMate/></div>
             <div className='trackmate-card'><RV/></div>
             <div className='trackmate-card'><BankApp/></div>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,6 +2,9 @@ import { NavLink } from "react-router-dom"
 import './App.css'
 import { FaHome } from "react-icons/fa";
 import LandingPage from "./LandingPage";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaPhoneSquareAlt } from "react-icons/fa";
 
 const NavBar = () =>{
 
@@ -13,12 +16,12 @@ const NavBar = () =>{
                 <FaHome color='white' size={25}/>
                 </NavLink>
                 </div>
+            {/* <div className='header'><NavLink to='https://www.linkedin.com/in/kaleb-davis-77037b336'><h1>Kaleb Davis</h1></NavLink></div>     */}
             <div className="links">
-                
                 <ul className="github-links">
-                    <li><NavLink to='https://github.com/Kalebsdavis'>Github</NavLink></li>
-                    <li><NavLink to='https://www.linkedin.com/in/kaleb-davis-77037b336'>LinkedIn</NavLink></li>
-                    <li><NavLink>801-680-8324</NavLink></li>
+                    <li><NavLink to='https://github.com/Kalebsdavis'><FaGithub size={20}/> Github</NavLink></li>
+                    <li><NavLink to='https://www.linkedin.com/in/kaleb-davis-77037b336'><FaLinkedin size={20}/>LinkedIn</NavLink></li>
+                    <li><NavLink><FaPhoneSquareAlt size={20}/>801-680-8324</NavLink></li>
                 </ul>
             </div>
         </div>
